0358be87025896574bba48f55b23f8c579d49878
ValeryShev <valeriashevtsova00@gmail.com>
2021-12-12 19:31:50 +0300

    lesson-4

--- main.js
+++ main.js
@@ -42,59 +42,89 @@
 // }
 
 
-
-
 // lesson-3
-function palindrome(str) {
+// function palindrome(str) {
+//
+//     if (str.length === 1) {
+//         return true;
+//     }
+//     if (str.length === 2) {
+//         return str[0] === str[1];
+//     }
+//     if (str[0] === str.slice (-1)) {
+//         return palindrome(str.slice(1, -1));
+//     }
+//     return false;
+// }
+// console.log (palindrome('шалаш'))
+//
+// function min(a, b) {
+//     if (a < b) {
+//         return a;
+//     }
+//     return b;
+// }
+// console.log(min(16, 70))
+//
+// function max(a, b) {
+//     if (a > b) {
+//         return a;
+//     }
+//     return b;
+// }
+// console.log(max(890, 70))
+//
+//
+//     min = 0;
+//     max = 100;
+//     let arr = [];
+//     for (let i = 0; i < 10; i++) {
+//         arr.push(Math.floor(Math.random() * (max - min) + min));
+//     }
+//     console.log(arr);
+// function strZero (arr2) {
+//     let newArr = [];
+//     arr2 = arr;
+//     for (let i = 0; i < arr2.length; i++) {
+//         if (arr2[i] % 10 === 0 || arr2[i] === 0) {
+//             arr2[i] = arr2[i].toString().replace('0', 'zero');
+//             console.log(arr2[i]);
+//             newArr.push(arr2[i]);
+//         } else {
+//             newArr.push(arr2[i]);
+//         }
+//     } return newArr;
+// }
+// console.log(strZero())
 
-    if (str.length === 1) {
-        return true;
-    }
-    if (str.length === 2) {
-        return str[0] === str[1];
-    }
-    if (str[0] === str.slice (-1)) {
-        return palindrome(str.slice(1, -1));
-    }
-    return false;
+
+//lesson-4
+function sum(a, b) {
+    return a+b
 }
-console.log (palindrome('шалаш'))
+console.log(sum(14, 7))
 
-function min(a, b) {
-    if (a < b) {
-        return a;
-    }
-    return b;
-}
-console.log(min(16, 70))
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+let baseColor = 0;
+document.getElementById("text1").addEventListener("click",
+    function newCol(){
+    baseColor++;
+    if (baseColor > colors.length) baseColor = 0;
+    const firstText = document.getElementById("text1")
+    firstText.style.color = colors[baseColor]
+});
+document.getElementById("text2").addEventListener("click",
+    function newCol(){
+        baseColor++;
+        if (baseColor > colors.length) baseColor = 0;
+        const secondText = document.getElementById("text2")
+        secondText.style.color = colors[baseColor]
+    });
+document.getElementById("text3").addEventListener("click",
+    function newCol(){
+        baseColor++;
+        if (baseColor > colors.length) baseColor = 0;
+        const thirdText = document.getElementById("text3")
+        thirdText.style.color = colors[baseColor]
+    });
 
-function max(a, b) {
-    if (a > b) {
-        return a;
-    }
-    return b;
-}
-console.log(max(890, 70))
-
-
-    min = 0;
-    max = 100;
-    let arr = [];
-    for (let i = 0; i < 10; i++) {
-        arr.push(Math.floor(Math.random() * (max - min) + min));
-    }
-    console.log(arr);
-function strZero (arr2) {
-    let newArr = [];
-    arr2 = arr;
-    for (let i = 0; i < arr2.length; i++) {
-        if (arr2[i] % 10 === 0 || arr2[i] === 0) {
-            arr2[i] = arr2[i].toString().replace('0', 'zero');
-            console.log(arr2[i]);
-            newArr.push(arr2[i]);
-        } else {
-            newArr.push(arr2[i]);
-        }
-    } return newArr;
-}
-console.log(strZero())
