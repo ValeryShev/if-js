28ff7b6f64a1eedaa6c10b5476802c78d45bc382
Lera Shevtsova <valeriashevtsova00@gmail.com>
2022-01-10 18:32:48 +0300

    C:/Program Files/Git/

--- main.js
+++ main.js
@@ -427,21 +427,21 @@ const obj3 = {
 };
 
 function isEqual(object1, object2) {
-        const props1 = Object.getOwnPropertyNames(object1);
-        const props2 = Object.getOwnPropertyNames(object2);
-        if (props1.length !== props2.length) {
+    const props1 = Object.getOwnPropertyNames(object1);
+    const props2 = Object.getOwnPropertyNames(object2);
+    if (props1.length !== props2.length) {
+        return false;
+    }
+    for (let i = 0; i < props1.length; i += 1) {
+        const prop = props1[i];
+        const bothAreObjects = typeof(object1[prop]) === 'object' && typeof(object2[prop]) === 'object';
+        if ((!bothAreObjects && (object1[prop] !== object2[prop]))
+            || (bothAreObjects && !isEqual(object1[prop], object2[prop]))) {
             return false;
         }
-        for (let i = 0; i < props1.length; i += 1) {
-            const prop = props1[i];
-            const bothAreObjects = typeof(object1[prop]) === 'object' && typeof(object2[prop]) === 'object';
-            if ((!bothAreObjects && (object1[prop] !== object2[prop]))
-                || (bothAreObjects && !isEqual(object1[prop], object2[prop]))) {
-                return false;
-            }
-        }
+    }
 
-        return true;
+    return true;
 }
 console.log(isEqual(obj1, obj2))
 console.log(isEqual(obj1, obj3))
